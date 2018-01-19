566725de6b6ad26ec64af5b67ac3f6f20b3cd1b2
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-19 11:20:08 +0900

    【発展学習3】Commentのパラメータ取得のストロングパラメータメソッドを追加"

--- MainCurriculum/lesson4/pictweet/app/controllers/comments_controller.rb
+++ MainCurriculum/lesson4/pictweet/app/controllers/comments_controller.rb
@@ -2,4 +2,9 @@ class CommentsController < ApplicationController
   def create
     Comment.create(text: params[:text], tweet_id: params[:tweet_id], user_id: current_user.id)
   end
+
+  private
+  def comment_params
+    params.permit(:text, :tweet_id)
+  end
 end
