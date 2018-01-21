9d044f749fc801c3d37222257d5cb1d347fbecda
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-22 01:11:06 +0900

    Question1: FizzBuzz問題

---
+++ ChallengesList/lesson1/question1.rb
@@ -0,0 +1,28 @@
+# 問題1. FizzBuzz問題
+# * 3で割り切れるときFizz
+# * 5で割り切れるときBuzz
+# * 15倍数のときFizzBuzz
+# * その他の場合、数をそのまま表示する
+# 
+def fizz_buzz
+  100.times do |index|
+
+    display_string = ""
+
+    if (index+1) % 3 == 0
+      display_string += "Fizz"
+    end
+
+    if (index+1) % 5 == 0
+      display_string += "Buzz"
+    end
+
+    if display_string.empty?
+      display_string = (index+1).to_s
+    end
+
+    puts display_string
+  end
+end
+
+fizz_buzz
