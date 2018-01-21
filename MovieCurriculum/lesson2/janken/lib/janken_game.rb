6dd9b2c879270786a754e9f24f61f2333162ea89
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-22 00:16:33 +0900

    じゃんけんの合いの手を整理して調整

--- MovieCurriculum/lesson2/janken/lib/janken_game.rb
+++ MovieCurriculum/lesson2/janken/lib/janken_game.rb
@@ -1,7 +1,7 @@
 require './janken'
 require './hand'
 
-def game(player, npc, janken)
+def janken_game(player, npc, janken)
   # じゃんけんで出し合う手を各々が決める
   player.hand_shape(rand(0..2))
   npc.hand_shape(rand(0..2))
@@ -9,16 +9,22 @@ def game(player, npc, janken)
   # じゃんけんの手を出し合う
   janken.battle(player.hand_out, npc.hand_out)
 
-  # じゃんけんの結果を表示する
-  result = janken.result
-  case result
-  when true
-    puts "プレイヤーの勝利"
-  when false
-    puts "NPCの勝利"
-  when nil
+  puts "プレイヤーの手\t: " + display_hand_shape(player.hand_out)
+  puts "NPCの手\t\t: " + display_hand_shape(npc.hand_out)
+
+  # じゃんけんの結果を返す
+  janken.result
+end
+
+def display_hand_shape(hand_of_player)
+  case hand_of_player
+  when 0
+    "グー！"
+  when 1
+    "チョキ！"
+  when 2
+    "パー！"
   end
-  result
 end
 
 # 出場者が互いに場に手を出す
@@ -26,11 +32,24 @@ player = Player::Hand.new
 npc = Player::Hand.new
 
 # じゃんけんゲームがスタート
+puts "最初はグー、じゃんけん..."
 janken = Game::Janken.new
 
 continue = true
 
 while continue do
-  result = game(player, npc, janken)
+  result = janken_game(player, npc, janken)
+
+  case result
+  when true
+    puts "****プレイヤーの勝利****"
+  when false
+    puts "****NPCの勝利****"
+  when nil
+    puts "あいこで…"
+    puts "----------------------"
+  end
+
+  # 結果が帰ってきていなければじゃんけん継続
   continue = result.nil?
 end
