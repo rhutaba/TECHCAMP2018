39274c023f8f7b14c5c6eb4e84807a5bc200c850
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-23 02:48:28 +0900

    メソッドのドキュメントを追加

--- MovieCurriculum/lesson2/janken/lib/hand.rb
+++ MovieCurriculum/lesson2/janken/lib/hand.rb
@@ -5,30 +5,50 @@ class Player
 
   HAND_SHAPE = [STONE, SCISSORS, PAPER]
 
+  #
+  # @param: [Integer] グー・チョキ・パー
+  # @return: [Integer|nil]
+  #
   def initialize(hand)
     if HAND_SHAPE.include?(hand)
       @hand = hand
     end
   end
 
+  #
+  # @return: [Integer|nil]
+  #
   def hand_out
     @hand
   end
 
+  #
+  # @param: [Integer]
+  # @return: [Integer]
+  #
   def recognize(others_hand)
     @others_hand = others_hand
   end
 
+  #
+  # @return: [Boolean]
+  #
   def draw?
     @hand == @others_hand
   end
 
+  #
+  # @return: [Boolean]
+  #
   def win?
     judge
   end
 
   private
 
+  #
+  # @return: [Boolean|nil]
+  #
   def judge
     case @hand
     when STONE
@@ -42,14 +62,23 @@ class Player
     end
   end
 
+  #
+  # @return: [Boolean]
+  #
   def stone_wins?
     @others_hand == SCISSORS
   end
 
+  #
+  # @return: [Boolean]
+  #
   def scissors_wins?
     @others_hand == PAPER
   end
 
+  #
+  # @return: [Boolean]
+  #
   def paper_wins?
     @others_hand == STONE
   end
