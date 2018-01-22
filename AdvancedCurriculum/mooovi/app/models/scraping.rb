e1e842c32ce1935e6f2c9938a7f1334d0bfe33e1
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-23 05:17:40 +0900

    テストページのページネーション全てからタイトルと画像URLを取得する機能を実装

--- AdvancedCurriculum/mooovi/app/models/scraping.rb
+++ AdvancedCurriculum/mooovi/app/models/scraping.rb
@@ -2,15 +2,28 @@ class Scraping
   def self.movie_urls
     puts 'get movies link URL'
 
+    links = []
     agent = Mechanize.new
-    current_url = "http://review-movie.herokuapp.com/"
-    current_page = agent.get(current_url)
+    root_url = "http://review-movie.herokuapp.com/"
+    next_url = ""
 
-    header_links = current_page.search('h2 a')
+    while true do
+      current_url = root_url + next_url
+      current_page = agent.get(current_url)
 
-    header_links.each do |header_link|
-      puts current_url + header_link[:href]
-      get_product(current_url + header_link[:href])
+      header_links = current_page.search('.entry-title a')
+
+      header_links.each do |header_link|
+        links << root_url + header_link[:href]
+      end
+
+      next_link = current_page.at('.pagination .next a')
+      break unless next_link
+      next_url = next_link[:href]
+    end
+    
+    links.each do |link|
+      get_product(link)
     end
   end
 
@@ -20,8 +33,8 @@ class Scraping
     agent = Mechanize.new
     page = agent.get(link)
 
-    title = page.at('.entry-title').inner_text
-    image_url = page.at('#entry-content-single img')[:src]
+    title = page.at('.entry-title').inner_text if page.at('.entry-title')
+    image_url = page.at('.entry-content img')[:src] if page.at('.entry-content img')
 
     product = Product.where(title: title, image_url: image_url).first_or_initialize
     product.save
