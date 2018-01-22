6800c6c8ffb57e4c9d6cb368f154b739ed0d9fcb
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-23 05:31:12 +0900

    スクレイピングで取得する情報に「監督」「あらすじ」「公開日」を追加

--- AdvancedCurriculum/mooovi/app/models/scraping.rb
+++ AdvancedCurriculum/mooovi/app/models/scraping.rb
@@ -35,8 +35,16 @@ class Scraping
 
     title = page.at('.entry-title').inner_text if page.at('.entry-title')
     image_url = page.at('.entry-content img')[:src] if page.at('.entry-content img')
+    director = page.at('.director span').inner_text if page.at('.director span')
+    detail = page.at('.entry-content p').inner_text if page.at('.entry-content p')
+    open_date = page.at('.date span').inner_text if page.at('.date span')
 
-    product = Product.where(title: title, image_url: image_url).first_or_initialize
+    product = Product.where(title: title).first_or_initialize
+    product.image_url = image_url
+    product.director = director
+    product.detail = detail
+    product.open_date = open_date
+
     product.save
   end
 end
