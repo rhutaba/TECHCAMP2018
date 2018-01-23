0c32b41e63c3a6f56d9530aca5921f347ba4333e
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-23 17:46:16 +0900

    topクラスを削除し、カリキュラム通りのbranchと同等の内容に修正

--- AdvancedCurriculum/mooovi/app/controllers/products_controller.rb
+++ AdvancedCurriculum/mooovi/app/controllers/products_controller.rb
@@ -1,6 +1,21 @@
-class ProductsController < ReviewController
+class ProductsController < ReviewsController
   def show
     # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
     @product = Product.find(params[:id])
+  end
+
+  def index
+    @products = Product.all.order('updated_at DESC').limit(20)
+  end
+
+  def search
+    # productsテーブルからキーワードで検索した作品データを20件取り出し、@products変数に入れる処理を書いて下さい
+    keyword = params[:keyword]
+    @products = Product.where('title LIKE(?)', "%#{keyword}%").limit(20)
+  end
+
+  private
+  def search_params
+    params.permit(:keyword)
   end
 end
---
+++ AdvancedCurriculum/mooovi/app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  layout 'review_site'
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = Review.new
+  end
+
+  def create
+    Review.create(review_params)
+    redirect_to controller: :products, action: :index
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:nickname, :rate, :review).merge(params.permit(:product_id))
+  end
+end
