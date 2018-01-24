3fe990f37110e08a4a4a8e432a590ef4e75e2580
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-24 18:51:12 +0900

    ボタンをクリックするとテキストの内容が変化する機能を実装

--- AdvancedCurriculum/techcamp_js/test-js/app/assets/javascripts/top.js
+++ AdvancedCurriculum/techcamp_js/test-js/app/assets/javascripts/top.js
@@ -1,7 +1,9 @@
 window.addEventListener("load", function() {
   var btn = document.querySelector("input");
 
+
   btn.addEventListener("click", function () {
-    console.log("Hello world");
+    var p = document.querySelector("p");
+    p.innerText = "クリックされた！";
   });
 });
