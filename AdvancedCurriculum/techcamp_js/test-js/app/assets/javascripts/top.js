e9c39d8fa15a870dd51c71f0ad084504a0ab524b
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-24 19:09:49 +0900

    クリックしたタブのテキストを表示する機能を追加

--- AdvancedCurriculum/techcamp_js/test-js/app/assets/javascripts/top.js
+++ AdvancedCurriculum/techcamp_js/test-js/app/assets/javascripts/top.js
@@ -2,5 +2,7 @@ window.addEventListener("load", function () {
   $("li").on("click", function () {
     $('li.selected').removeClass("selected");
     $(this).addClass("selected");
+    $('.contents div').hide();
+    $('.' + this.id).show();
   });
 });
