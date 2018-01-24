858937954e061b0c15083a085ec7791cd1b00131
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-24 18:40:40 +0900

    JSの関数printHelloWithButtonを実装

--- AdvancedCurriculum/techcamp_js/test-js/app/assets/javascripts/top.js
+++ AdvancedCurriculum/techcamp_js/test-js/app/assets/javascripts/top.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+function printHelloWithButton() {
+  var btn = document.querySelector("input");
+
+  function printHello() {
+    console.log("Hello world");
+  }
+
+  btn.addEventListener("click", printHello);
+}
+
+window.addEventListener("load", printHelloWithButton);
