421bad74b4ceb0e4e646e6a572eee877948c6dad
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-24 19:11:45 +0900

    windowのloadイベントの記載をjqueryに変更

--- AdvancedCurriculum/techcamp_js/test-js/app/assets/javascripts/top.js
+++ AdvancedCurriculum/techcamp_js/test-js/app/assets/javascripts/top.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+$(window).on("load", function () {
   $("li").on("click", function () {
     $('li.selected').removeClass("selected");
     $(this).addClass("selected");
