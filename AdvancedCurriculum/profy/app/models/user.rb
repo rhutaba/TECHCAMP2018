3c6fc117776426ec13c3db0b8b24561450918679
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-29 02:28:24 +0900

    paper_clipをUserモデルにアタッチ

--- AdvancedCurriculum/profy/app/models/user.rb
+++ AdvancedCurriculum/profy/app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          authentication_keys: [:email, :group_key]
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
+  validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
+
   belongs_to :group
 
   #validation
