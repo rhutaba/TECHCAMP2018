f805a6d5da008df693431c2d4a5ea69e0d9130ae
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-29 00:31:46 +0900

    Groupモデルを作成

--- AdvancedCurriculum/profy/db/schema.rb
+++ AdvancedCurriculum/profy/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126090332) do
+ActiveRecord::Schema.define(version: 20180128153015) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "key",        limit: 255
+    t.text     "detail",     limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
