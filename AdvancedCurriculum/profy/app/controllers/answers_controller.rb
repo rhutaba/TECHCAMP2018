5a84e6cc9fe332b0000a60c0ff5340aec4c8746a
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-01-31 23:51:11 +0900

    回答済みの質問に答えられないよう判定とリダイレクトを設定

--- AdvancedCurriculum/profy/app/controllers/answers_controller.rb
+++ AdvancedCurriculum/profy/app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+  before_action :redirect, only: :new
+
   def new
     @question = Question.find(params[:question_id])
     @answer = Answer.new
@@ -13,5 +15,11 @@ class AnswersController < ApplicationController
 
   def create_params
     params.require(:answer).permit(:question_id, :text).merge(user_id: current_user.id)
+  end
+
+  def redirect
+    if Answer.exists?(question_id: params[:question_id], user_id: current_user.id)
+      redirect_to :root
+    end
   end
 end
