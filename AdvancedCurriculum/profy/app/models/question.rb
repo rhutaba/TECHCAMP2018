4f37fa24302d8441374f96875b7af3a7b726a605
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-02-02 09:43:12 +0900

    タイムライン用にFeedContentモデルを作成

--- AdvancedCurriculum/profy/app/models/question.rb
+++ AdvancedCurriculum/profy/app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
   has_many :answers
+  has_one :feed_content, as: :content, dependent: :destroy
 
   validates_presence_of :user_id, :text, :group_id
 
