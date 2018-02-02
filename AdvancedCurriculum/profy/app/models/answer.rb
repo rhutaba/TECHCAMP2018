9034ad58144e2c1d94c4886e536b2d2617e81665
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-02-02 11:22:17 +0900

    feed_contentsを使ってタイムラインに回答を表示

--- AdvancedCurriculum/profy/app/models/answer.rb
+++ AdvancedCurriculum/profy/app/models/answer.rb
@@ -5,12 +5,18 @@ class Answer < ActiveRecord::Base
 
   #callback
   after_create :create_feed_content
+  after_update :update_feed_content
 
   #validation
   validates_presence_of :user_id, :text
 
   private
+
   def create_feed_content
     self.feed_content = FeedContent.create(group_id: question.group_id, updated_at: updated_at)
+  end
+
+  def update_feed_content
+    self.feed_content.update(updated_at: updated_at)
   end
 end
