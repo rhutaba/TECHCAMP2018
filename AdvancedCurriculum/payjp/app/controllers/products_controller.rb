3f810dc67a7ab1a137592aec8091c0bf9ac65289
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-02-06 17:30:03 +0900

    環境変数にテストカード情報を登録し、環境変数を読み込む形に変更

--- AdvancedCurriculum/payjp/app/controllers/products_controller.rb
+++ AdvancedCurriculum/payjp/app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
+#末尾に以下のアクションを追加
   def purchase
-    Payjp.api_key = PAYJP_SECRET_KEY
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       :amount => 3500,
       :card => params['payjp-token'],
--- AdvancedCurriculum/payjp/config/initializers/setting.rb
+++
@@ -1,5 +0,0 @@
-# テスト環境用公開可能鍵
-PAYJP_PUBLIC_KEY='pk_test_986f31102d46da2a500e5069'
-
-# テスト環境用非公開鍵
-PAYJP_SECRET_KEY='sk_test_e2f9245ab22f907c1047771b'
