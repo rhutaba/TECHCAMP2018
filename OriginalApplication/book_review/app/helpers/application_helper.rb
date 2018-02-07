bf4e72e7865b623b7bcf7b8a8d2c713bc1b5d946
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-02-07 10:49:10 +0900

    headerとfooterの雛形を作成

--- OriginalApplication/book_review/app/helpers/application_helper.rb
+++ OriginalApplication/book_review/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def copyright
+    text = 'Copyright (c) 2018'
+    if Date.today.year > 2018
+      text << '-' + Date.today.year.to_s
+    end
+    text << ' project_j , All Rights Reserved.'
+    text
+  end
 end
