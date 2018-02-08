3579b4b9883f4a38f0e98011936d1c2ed53d42c7
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-02-08 17:47:08 +0900

    UserReviewsControllerとルーティングを設定

---
+++ OriginalApplication/book_review/app/controllers/user_review_controller.rb
@@ -0,0 +1,4 @@
+class UserReviewController < ApplicationController
+  def show
+  end
+end
--- OriginalApplication/book_review/config/routes.rb
+++ OriginalApplication/book_review/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'top#index'
   resources :inquiry, only: [:new, :create]
+  resources :user_review, only: [:show]
 end
