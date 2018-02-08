70aa9ec8cde08e301a35a4c466812afdcb3e4e55
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-02-08 10:47:59 +0900

    お問合わせページのレイアウトを作成

--- OriginalApplication/book_review/app/controllers/inquiry_controller.rb
+++ OriginalApplication/book_review/app/controllers/inquiry_controller.rb
@@ -2,4 +2,15 @@ class InquiryController < ApplicationController
   def new
     @inquiry = Inquiry.new
   end
+
+  def create
+    Inquiry.create(inquiry_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:email, :user_name, :target_url, :inquiry_text)
+  end
 end
