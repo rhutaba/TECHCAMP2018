99a6af99b1c5973393fae02a7af55cc57d6df6ba
Juntetsu TEI <juntetsu.tei@crowdworks.co.jp>
2018-02-11 17:04:55 +0900

    検索機能と検索結果の一覧を実装

--- OriginalApplication/book_review/config/routes.rb
+++ OriginalApplication/book_review/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'top#index'
   resources :inquiry, only: [:new, :create]
-  resources :books, only: [:index]
+  resources :books, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end
   resources :user_review, only: [:show]
 end
